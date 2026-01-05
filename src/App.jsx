a323da0e925a3d9db0ac9ffb35a2f1f48de5569c
prinnasit <prinnasit@gmail.com>
2026-01-05 14:48:17 +0700

    feat: implement counter state nd github api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import ProfileCard from './components/ProfileCard'
 function App() {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
-      <h1>My Team Portfolio</h1>
+  const [githubData , setGitHubData] = useState(null);
+  const username = "prinnasit";
 
-      {/* ลองส่ง Props ข้อมูลของตัวเองลงไป */}
-      <ProfileCard
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(data => {
+        setGitHubData(data) })
+      .catch(err => console.error(err));
+  } , []);
+
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h1>My First React App</h1>
+
+      {/* { ลองส่ง Props ข้อมูลของตัวเองลงไป } */}
+      {/* <ProfileCard
         name="Prinnasit Jampa"
         role="Student @ CEDT"
         bio="play hard, study hard"
-      />
+      /> */}
 
       {/* ลองเรียกใช้ซ้ำอีกครั้งด้วยข้อมูลที่ต่างออกไป */}
-      <ProfileCard
+      {/* <ProfileCard
         name="John Doe"
         role="Guest Developer"
         bio="I love coding and learning "
-      />
+      /> */} 
+
+      {githubData ? (
+        <ProfileCard
+          name={githubData.name || githubData.login}
+          role="GitHub User"
+          bio={githubData.bio || "No bio available."}
+        />
+      ) : (
+        <p>Loading GitHub data...</p>
+      )}
     </div>
   );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,12 +1,21 @@
 // src/components/ProfileCard.jsx
+
+import { useState } from "react";
 function ProfileCard({ name, role, bio }) {
-  return (
-    <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '10px' }}>
-      <h2>{name}</h2>
-      <p><strong>Role:</strong> {role}</p>
-      <p>{bio}</p>
-    </div>
-  );
+
+    const [likes, setLikes] = useState(0);  
+
+    return (
+        <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '10px' }}>
+        <h2>{name}</h2>
+        <p><strong>Role:</strong> {role}</p>
+        <p>{bio}</p>
+
+        <button onClick={() => setLikes(likes + 1)}>
+            Like ({likes})
+        </button>
+        </div>
+    );
 }
 
 export default ProfileCard;
